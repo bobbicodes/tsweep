01a5a6e153c5b2959ae20bab1cad04e36f15588e
Bobby Towers <sdfwer124@gmail.com>
2017-11-05 03:57:41 -0800

    New version with variable size grid!

--- core.clj
+++ core.clj
@@ -1,45 +1,157 @@
 (ns mine.core
   (:gen-class))
 
-;; We define a map of squares,
-;; each key corresponding to a vector of its surrounding squares. 
-(declare square)
+;; create board:
+;to be changed by user
+(def rows 12)
 
-(def squares
-  {0 [1 8 9] 1 [0 2 8 9 10] 2 [1 3 9 10 11] 3 [2 4 10 11 12]
-   4 [3 5 11 12 13] 5 [4 6 12 13 14] 6 [5 7 13 14 15] 7 [6 14 15]
-   8 [0 1 9 16 17] 9 [0 1 2 8 10 16 17 18] 10 [1 2 3 9 11 17 18 19]
-   11 [2 3 4 10 12 18 19 20] 12 [3 4 5 11 13 19 20 21]
-   13 [4 5 6 12 14 20 21 22] 14 [5 6 7 13 15 21 22 23]
-   15 [6 7 14 22 23] 16 [8 9 17 24 25] 17 [8 9 10 16 18 24 25 26]
-   18 [9 10 11 17 19 25 26 27] 19 [10 11 12 18 20 26 27 28]
-   20 [11 12 13 19 21 27 28 29] 21 [12 13 14 20 22 28 29 30]
-   22 [13 14 15 21 23 29 30 31] 23 [14 15 22 30 31] 24 [16 17 25 32 33]   25 [16 17 18 24 26 32 33 34] 26 [17 18 19 25 27 33 34 35]
-   27 [18 19 20 26 28 34 35 36] 28 [19 20 21 27 29 35 36 37]
-   29 [20 21 22 28 30 36 37 38] 30 [21 22 23 29 30 37 38 39]
-   31 [22 23 30 38 39] 32 [24 25 33 40 41] 33 [24 25 26 32 34 40 41 42]   34 [25 26 27 33 35 41 42 43] 35 [26 27 28 34 36 42 43 44]
-   36 [27 28 29 35 37 43 44 45] 37 [28 29 30 36 38 44 45 46]
-   38 [29 30 31 37 39 45 46 47] 39 [30 31 38 46 47] 40 [32 33 41 48 49]   41 [32 33 34 40 42 48 49 50] 42 [33 34 35 41 43 49 50 51]
-   43 [34 35 36 42 44 50 51 52] 44 [35 36 37 43 45 51 52 53]
-   45 [36 37 38 44 46 52 53 54] 46 [37 38 39 45 47 53 54 55]
-   47 [38 39 46 54 55] 48 [40 41 49 56 57] 49 [40 41 42 48 50 56 57 58]   50 [41 42 43 49 51 57 58 59] 51 [42 43 44 50 52 58 59 60]
-   52 [43 44 45 51 53 59 60 61] 53 [44 45 46 52 54 60 61 62]
-   54 [45 46 47 53 55 61 62 63] 55 [46 47 54 62 63] 56 [48 49 57]
-   57 [48 49 50] 58 [49 50 51 57 59] 59 [50 51 52 58 60] 
-   60 [51 52 53 59 61] 61 [52 53 54 60 62] 62 [53 54 55 61 63]
-   63 [54 55 62]})
+(defn create-board [rows set]
+    (if (= (* rows rows) (count set))
+        set
+        (recur rows (conj set #{}))))
 
-; To retrieve any square's vector:
-; user> (squares 0)
-; [1 8 9]
+(def board (create-board rows [#{}]))
 
-;; populate the board with mines
+(defn l-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (not= 0 (rem square rows))
+            (recur (assoc board square (conj (get board square) (dec square))) (dec square))
+(recur board (dec square))))))
 
-(defn add-mine [set] (conj set (int (rand 64))))
+(defn b-add [coll]
+ (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+board
+(if (> (- (* rows rows) rows) square)
+(recur (assoc board square (conj (get board square) (+ rows square))) (dec square))
+(recur board (dec square))))))
 
-(defn set-mines [] (add-mine (add-mine (add-mine (add-mine (add-mine (add-mine (add-mine (add-mine #{})))))))))
+(defn bl-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (> (- (* rows rows) rows)  square) (not= 0 (rem square rows)))
+          (recur (assoc board square (conj (get board square) (dec (+ rows square)))) (dec square))
+(recur board (dec square))))))
 
-(def mines (set-mines))
+(defn br-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (> (- (* rows rows) rows)  square) (not= 0 (rem (inc square) rows)))
+          (recur (assoc board square (conj (get board square) (inc (+ rows square)))) (dec square))
+(recur board (dec square))))))
+
+(defn r-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (not= 0 (rem (inc square) rows))
+            (recur (assoc board square (conj (get board square) (inc square))) (dec square))
+(recur board (dec square))))))
+
+(defn t-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (< rows (inc square))
+            (recur (assoc board square (conj (get board square) (- square rows))) (dec square))
+(recur board (dec square))))))
+
+(defn tl-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (< rows (inc square)) (not= 0 (rem square rows)))
+          (recur (assoc board square (conj (get board square) (dec (- square rows)))) (dec square))
+(recur board (dec square))))))
+
+(defn tr-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (< rows (inc square)) (not= 0 (rem (inc square) rows)))
+          (recur (assoc board square (conj (get board square) (inc (- square rows)))) (dec square))
+(recur board (dec square))))))(defn l-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (not= 0 (rem square rows))
+            (recur (assoc board square (conj (get board square) (dec square))) (dec square))
+(recur board (dec square))))))
+
+(defn b-add [coll]
+ (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+board
+(if (> (- (* rows rows) rows) square)
+(recur (assoc board square (conj (get board square) (+ rows square))) (dec square))
+(recur board (dec square))))))
+
+(defn bl-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (> (- (* rows rows) rows)  square) (not= 0 (rem square rows)))
+          (recur (assoc board square (conj (get board square) (dec (+ rows square)))) (dec square))
+(recur board (dec square))))))
+
+(defn br-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (> (- (* rows rows) rows)  square) (not= 0 (rem (inc square) rows)))
+          (recur (assoc board square (conj (get board square) (inc (+ rows square)))) (dec square))
+(recur board (dec square))))))
+
+(defn r-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (not= 0 (rem (inc square) rows))
+            (recur (assoc board square (conj (get board square) (inc square))) (dec square))
+(recur board (dec square))))))
+
+(defn t-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (< rows (inc square))
+            (recur (assoc board square (conj (get board square) (- square rows))) (dec square))
+(recur board (dec square))))))
+
+(defn tl-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (< rows (inc square)) (not= 0 (rem square rows)))
+          (recur (assoc board square (conj (get board square) (dec (- square rows)))) (dec square))
+(recur board (dec square))))))
+
+(defn tr-add [coll]
+        (loop [board coll square (dec (count coll))]
+    (if (> 0 square)
+        board
+        (if (and (< rows (inc square)) (not= 0 (rem (inc square) rows)))
+          (recur (assoc board square (conj (get board square) (inc (- square rows)))) (dec square))
+(recur board (dec square))))))
+
+(def squares  (tl-add (t-add (tr-add (l-add (r-add (bl-add (b-add (br-add board)))))))))
+
+;; populate the board with mines:
+
+(defn set-mines [set n squares] 
+  (if (= n (count set))
+    set
+        (recur (conj set (int (rand squares))) n squares)))
+
+
+;this will be changed later to be entered by the user
+(def num-mines 10)
+
+(def mines (set-mines #{} num-mines (count squares)))
 
 ;; mine detector calculates how many mines are around each square
 
@@ -75,26 +187,88 @@
 (str "  " (colorize (str (mine-detector square) ) :blue) "  "))))
 
 (defn full [square]
-(if (> 10 square)
-(str (colorize "XX" :green) square (colorize "XX" :green))
-    (str (colorize "X" :green) square (colorize "XX" :green))))
+  (if (> 10 square)
+      (str (colorize "XX" :green) square (colorize "XX" :green))
+(if (< 99 square)
+  (str (colorize "X" :green) square (colorize "X" :green))      
+  (str (colorize "X" :green) square (colorize "XX" :green)))))
 
 (defn full-blank [square] (str (colorize "XXXXX" :green)))
 (defn clear-blank [square] (str "     "))
 (def mined (colorize "  !!!  " :red))
 
-
-
-
 (defn paint-blank [square]
-(if (contains? stepped square)
+  (if (contains? stepped square)
       (if (contains? mines square) mined (clear-blank square)) (full-blank square)))
 
 (defn paint [square]
   (if (contains? stepped square)
-      (if (contains? mines square) mined (clear square)) (full square)))
+      (if (contains? mines square)
+          mined
+          (clear square))
+    (full square)))
 
-(defn painter [] (do (println (map paint-blank (range 8))) (println (map paint (range 8))) (println (map paint-blank (range 8))) (println (map paint-blank (range 8 16))) (println (map paint (range 8 16))) (println (map paint-blank (range 8 16))) (println (map paint-blank (range 16 24))) (println (map paint (range 16 24))) (println (map paint-blank (range 16 24))) (println (map paint-blank (range 24 32))) (println (map paint (range 24 32))) (println (map paint-blank (range 24 32))) (println (map paint-blank (range 32 40))) (println (map paint (range 32 40))) (println (map paint-blank (range 32 40))) (println (map paint-blank (range 40 48))) (println (map paint (range 40 48))) (println (map paint-blank (range 40 48))) (println (map paint-blank (range 48 56))) (println (map paint (range 48 56))) (println (map paint-blank (range 48 56))) (println (map paint-blank (range 56 64))) (println (map paint (range 56 64))) (println (map paint-blank (range 56 64)))))
+;; need a new painter func to show correct num squares.
+
+; It needs to:
+; Divide the board into rows.
+; paint each row.
+
+; partition board
+
+(def board-rows (partition rows squares))
+
+(defn painter []
+  (println (map paint-blank (range rows)))
+  (println (map paint (range rows)))
+  (println (map paint-blank (range rows)))
+(println (map paint-blank (range 12 24)))
+  (println (map paint (range 12 24)))
+  (println (map paint-blank (range 12 24)))
+(println (map paint-blank (range 24 36)))
+  (println (map paint (range 24 36)))
+  (println (map paint-blank (range 24 36)))
+(println (map paint-blank (range 36 48)))
+  (println (map paint (range 36 48)))
+  (println (map paint-blank (range 36 48)))
+(println (map paint-blank (range 48 60)))
+  (println (map paint (range 48 60)))
+  (println (map paint-blank (range 48 60)))
+(println (map paint-blank (range 60 72)))
+  (println (map paint (range 60 72)))
+  (println (map paint-blank (range 60 72)))
+(println (map paint-blank (range 72 84)))
+  (println (map paint (range 72 84)))
+  (println (map paint-blank (range 72 84)))
+(println (map paint-blank (range 84 96)))
+  (println (map paint (range 84 96)))
+  (println (map paint-blank (range 84 96)))
+(println (map paint-blank (range 96 108)))
+  (println (map paint (range 96 108)))
+  (println (map paint-blank (range 96 108)))
+(println (map paint-blank (range 108 120)))
+  (println (map paint (range 108 120)))
+  (println (map paint-blank (range 108 120)))
+(println (map paint-blank (range 120 132)))
+  (println (map paint (range 120 132)))
+  (println (map paint-blank (range 120 132)))
+(println (map paint-blank (range 132 144)))
+  (println (map paint (range 132 144)))
+  (println (map paint-blank (range 132 144)))
+)
+
+
+
+;(defn painter []
+;  (do
+;
+;    (println (map paint (range 2)))
+;    (println (map paint-blank (range 2)))
+;    (println (map paint-blank (range 2 4)))
+;    (println (map paint (range 2 4)))
+;    (println (map paint-blank (range 2 4)))))
+
+
 
 (defn step [square] (do (def stepped (conj stepped square)) (painter)))
 
@@ -114,7 +288,7 @@
 
 
 (defn prompt-step [] 
-  (if (= 64 (+ (count stepped) (count mines))) 
+  (if (= (count squares) (+ (count stepped) (count mines))) 
 (do (println "8888888888  88      88  8888888888  88       88")
 (println "88          88      88  888         88      88")
 (println "88          88      88  88          88     88")
@@ -139,7 +313,7 @@
       
  (if (< 0 (count ((fn [a b] (set (filter #(contains? b %) a))) stepped mines)))
             (println "You died. Fucking asshole!")
-(do (println "Tread lightly, if you dare. [0-63]")
+(do (println "Tread lightly, if you dare...")
                 (let [square (Integer. (get-input))] (step square))
                 (prompt-step)))))
       
