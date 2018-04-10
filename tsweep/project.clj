9f4354c91abd0db9d188b4595bf5304d58a9fa6f
Bobby Towers <porkostomus@mail.com>
2018-04-09 22:28:15 -0700

    leiningen project

---
+++ tsweep/project.clj
@@ -0,0 +1,9 @@
+(defproject tsweep "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot tsweep.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
